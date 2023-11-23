62bb574467b3b9a35e9a4b687639fc39d827582e
JAOUAD OUXHAIB s306971 <s306971@studenti.polito.it>
2023-11-23 12:19:45 +0100

    added loading spinner

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import DarkModeButton from './components/DarkModeButton';
 import Dashboard from './components/Dashboard';
+import Error from './components/Error';
 import SearchForm from './components/SearchForm';
 import { CountryData } from './utils/types';
 
@@ -10,12 +11,14 @@ function App() {
     const [loading, setLoading] = useState<boolean>(false)
     const [darkMode, setDarkMode] = useState<boolean>(false)
     const [countryData, setCountryData] = useState<CountryData|undefined>(undefined)
+    const [error, setError] = useState<boolean|undefined>(undefined)
 
 
     
   return (
     <div className={`app ${darkMode? `dark`:''} `}>
-      <SearchForm setLoading={setLoading} setCountryData={setCountryData}/>
+      <SearchForm loading={loading} setLoading={setLoading} setCountryData={setCountryData} setError={setError}/>
+      {error && <Error />}
       {countryData && <Dashboard  loading={loading} countryData={countryData}/>}
       <DarkModeButton darkMode={darkMode} setDarkMode={setDarkMode}  />
     </div>
