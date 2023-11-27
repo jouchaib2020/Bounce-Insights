6109fc254da9b55f8637626bb6c55c1d10e325d9
JAOUAD OUXHAIB s306971 <s306971@studenti.polito.it>
2023-11-27 15:12:39 +0100

    removed to .env  and regenerated exposed credentials

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -31,7 +31,7 @@ export default function Map({latlng, name}: {latlng: number[]; name: string}){
   return (
     <div className='w-4/12 h-96 border border-gray-600 rounded-lg p-1'>
       <GoogleMap
-        bootstrapURLKeys={{ key: "AIzaSyCfRAhw-Oz4a_uL9gmGQxeNE--vLIQ261w"}}
+        bootstrapURLKeys={{ key: import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string}}
         onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
         center={defaultProps.center}
         zoom={defaultProps.zoom}
